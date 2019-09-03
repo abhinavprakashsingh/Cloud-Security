3bab8692b00d422aab6717079745becf73a5edb7
inside-abhinav <abhinav.inside@hotmail.com>
2019-09-03 14:52:08 +0530

    Add files via upload

---
+++ database_encr/MyDBConnection.java
@@ -0,0 +1,76 @@
+/*
+ * MyDBConnection.java
+ *
+ * Created on 2005/01/16, 10:50
+ */
+
+package database_encr;
+
+import java.sql.*;
+
+public class MyDBConnection {
+   
+    private Connection myConnection;
+    
+    /** Creates a new instance of MyDBConnection */
+    public MyDBConnection() {
+
+    }
+
+    public void init(){
+    
+       try{
+        
+        Class.forName("com.mysql.jdbc.Driver");
+        myConnection=DriverManager.getConnection(
+                "jdbc:mysql://localhost/cloud-sec","root", ""
+                );
+        }
+        catch(Exception e){
+            System.out.println("Failed to get connection");
+            e.printStackTrace();
+        }
+    }
+    
+    
+    public Connection getMyConnection(){
+        return myConnection;
+    }
+    
+    
+    public void close(ResultSet rs){
+        
+        if(rs !=null){
+            try{
+               rs.close();
+            }
+            catch(Exception e){}
+        
+        }
+    }
+    
+     public void close(java.sql.Statement stmt){
+        
+        if(stmt !=null){
+            try{
+               stmt.close();
+            }
+            catch(Exception e){}
+        
+        }
+    }
+     
+  public void destroy(){
+  
+    if(myConnection !=null){
+    
+         try{
+               myConnection.close();
+            }
+            catch(Exception e){}
+        
+        
+    }
+  }
+    
+}
